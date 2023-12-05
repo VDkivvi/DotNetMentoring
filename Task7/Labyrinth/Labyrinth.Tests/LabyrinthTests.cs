9ef11e46d3e2f07a66efb3f835dc10e4d40ef9a2
VDkivvi <valeriya.danilova+github@gmail.com>
2023-12-05 19:27:57 +0200

    Kata 2.Add labyrinth validation
    Added validation to have a proper grid

--- Task7/Labyrinth/Labyrinth.Tests/LabyrinthTests.cs
+++ Task7/Labyrinth/Labyrinth.Tests/LabyrinthTests.cs
@@ -4,5 +4,75 @@ namespace Labyrinth.Tests
     public class LabyrinthTests
     {
 
+        [TestMethod]
+        public void Labyrinth_Validate_Rows()
+        {
+            var l = new char[,] {{ '*', 's', '*', '-', 'e', '*' },};
+            _ = Assert.ThrowsException<ArgumentException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_Cols()
+        {
+            var l = new char[,]
+               {{ '*' },
+                { 's' },
+                { '*' },
+                { '-' },
+                { '*' },
+                { 'e' },
+                { '*' }};
+            Assert.ThrowsException<ArgumentException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_Start()
+        {
+            var l = new char[,]
+            {{ '*', '*', '*', '*', 'e', '*' },
+             { '*', '-', '*', '*', '-', '*' },
+             { '*', '-', '-', '*', '*', '*' },};
+            Assert.ThrowsException<ApplicationException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_End()
+        {
+            var l = new char[,]
+            {{ '*', '*', '*', '*', '-', '*' },
+             { '*', '-', '*', '*', '-', '*' },
+             { '*', '-', '-', '*', 's', '*' },};
+            Assert.ThrowsException<ApplicationException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_Wrong_Symbols()
+        {
+            var l = new char[,]
+            {{ '*', 'e', '*', '*', '-', '*' },
+             { '*', '-', '*', 'i', '-', '*' },
+             { '*', '-', '-', '*', 's', '*' },};
+            Assert.ThrowsException<ApplicationException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_Several_Starts()
+        {
+            var l = new char[,]
+            {{ '*', 's', '*', '*', '-', '*' },
+             { '*', '-', '*', '-', '-', '*' },
+             { '*', '-', '-', '*', 's', 'e' },};
+            Assert.ThrowsException<ApplicationException>(() => { new Labyrinth(l); });
+        }
+
+        [TestMethod]
+        public void Labyrinth_Validate_Several_Ends()
+        {
+            var l = new char[,]
+            {{ '*', 'e', '*', '*', '-', '*' },
+             { '*', '-', '*', '-', '-', '*' },
+             { '*', '-', '-', '*', 's', 'e' },};
+            Assert.ThrowsException<ApplicationException>(() => { new Labyrinth(l); });
+        }
     }
 }
--- Task7/Labyrinth/Labyrinth/Labyrinth.cs
+++ Task7/Labyrinth/Labyrinth/Labyrinth.cs
@@ -13,6 +13,15 @@ public class Labyrinth : ILabyrinth
 
     public Labyrinth(char[,] data)
     {
+        _labyrinth = data;
+        _rows = data.GetLength(0);
+        _columns = data.GetLength(1);
+        Validate();
+
+        _labyrinthPoints = new Point[_rows, _columns];
+        for (var i = 0; i < _rows; i++)
+            for (var j = 0; j < _columns; j++)
+                _labyrinthPoints[i, j] = new Point((i, j)) { Value = _labyrinth[i, j] };
     }
 
     public (int row, int column) FindCharPosition(char ch)
@@ -30,4 +39,19 @@ public class Labyrinth : ILabyrinth
         throw new NotImplementedException();
     }
 
+    private void Validate()
+    {
+        var flatten = _labyrinth.Cast<char>();
+
+        if (_labyrinth == null)
+            throw new ArgumentNullException(paramName: nameof(_labyrinth));
+        if (_rows <= 1 || _columns <= 1)
+            throw new ArgumentException("Wrong number of cells. Should be more than 2 rows and columns");
+        if (flatten.Distinct().Any(s => Array.IndexOf(_availableChars, s) == -1))
+            throw new ApplicationException("The Labyrinth contains not allowed cell values");
+        if (!flatten.Distinct().Contains('s') || !flatten.Distinct().Contains('e'))
+            throw new ApplicationException("There's no start or end in the labyrinth");
+        if (flatten.Where(x => x == 's').Count() > 1 || flatten.Where(x => x == 'e').Count() > 1)
+            throw new ApplicationException("There are several starting or ending points in the labyrinth");
+    }
 }
